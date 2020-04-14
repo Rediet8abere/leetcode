dae97513da7affb681f094b4796011ca283a637e
Rediet8abere <redietaberegesesse@gmail.com>
2020-04-13 23:08:04 -0700

    3sum

--- 3Sum.py
+++ 3Sum.py
@@ -3,37 +3,22 @@ def threeSum(nums):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-
-        threesum = []
-
-        if len(nums)==0 or nums==None:
-            return threesum
         nums = sorted(nums)
-        print("nums", nums)
+        three_sum = set()
         for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
-                    print("to pass", nums[i], nums[i-1])
-                    continue
-            left = i+1
-            right = len(nums)-1
-            while left<right:
-                sum = nums[left]+nums[right]+nums[i]
-                print("sum", sum)
-                print(nums[left], nums[right], nums[i])
-                if sum<0:
-                    left+=1
-                elif sum<right:
-                    right-=1
+            low = i+1
+            high = len(nums)-1
+            sum = 0 - nums[i]
+            while low < high:
+                if nums[low] + nums[high] == sum:
+
+                    three_sum.add((nums[i], nums[low], nums[high]))
+                    low += 1
+                    high -= 1
+                elif nums[low] + nums[high] > sum:
+                    high -= 1
                 else:
-                    threesum.append([nums[left], nums[right], nums[i]])
-                    while left<right and nums[left]==nums[left+1]:
-                        left = left + 1
-                    while left<right and nums[right]==nums[right-1]:
-                        right = right + 1
-                    left+=1
-                    right-=1
-        return threesum
+                    low += 1
+        return three_sum
 
-
-
-print(threeSum([-1, 0, 1, 2, -1, -4]))
+print(threeSum([-1, 0, 1, 2, -1, -4, -1]))
